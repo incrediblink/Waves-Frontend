3fefdf96997c99b59e2b86eef16e154a9828b476
IncredibLink <incrediblelink@gmail.com>
2016-08-09 01:55:21 +0800

    重新写了 Timeline，增加了还是未成品的 Header Image
    通过非常丑陋的方式…
    和非常丑陋的界面……

--- src/app/event/event.component.ts
+++ src/app/event/event.component.ts
@@ -17,16 +17,16 @@ export class EventComponent implements OnInit {
 
     public collections: Array<any> = [
         {
-            title: "迟迟笑了",
-            abstract: "拿到商家拿来封口的 1TB 硬盘后，迟迟笑了",
-            time: "2016年8月8日10:51:14",
-            link: "cn.niuyaotimes.com",
-            source: "扭腰时报中文网"
+            title: '迟迟笑了',
+            abstract: '拿到商家拿来封口的 1TB 硬盘后，迟迟笑了',
+            time: '2016年8月8日10:51:14',
+            link: 'https://cn.niuyaotimes.com',
+            source: '扭腰时报中文网'
         },{
-            title: "惨！豆蔻少年网上购买 U 盘收到后发现是纸糊的！",
-            time: "2016年8月7日10:52:49",
-            link: "renming.com",
-            source: "人名日报"
+            title: '惨！豆蔻少年网上购买 U 盘收到后发现是纸糊的！',
+            time: '2016年8月7日10:52:49',
+            link: 'http://renming.com',
+            source: '人名日报'
         }
     ];
 }
