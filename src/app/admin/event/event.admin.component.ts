fbef70f6bc5c37a0f055f513a499105ccdcd4f0b
IncredibLink <incrediblelink@gmail.com>
2016-09-17 19:13:05 +0800

    增加上传事件标题图的页面

--- src/app/admin/event/event.admin.component.ts
+++ src/app/admin/event/event.admin.component.ts
@@ -1,18 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../../service/event';
+import { UploadService } from '../../service/upload';
 
 @Component({
   selector: 'my-event-admin',
   templateUrl: './event.admin.component.html',
   styleUrls: ['./event.admin.component.scss'],
-  providers: [EventService]
+  providers: [EventService, UploadService]
 })
 export class EventAdminComponent implements OnInit {
+
     public tab = {
         addEvent: {
             active: true
         },
         addNews: {
+            active: false
+        },
+        uploadImage: {
             active: false
         }
     };
@@ -37,19 +42,39 @@ export class EventAdminComponent implements OnInit {
         id: null,
         url: null
     }
-    
-    public addNews = (id, url) => this.eventService.addNews(id, url)
-                                    .subscribe(
-                                        data => {
-                                            this.add.id = null;
-                                            this.add.url = null;
-                                        },
-                                        err => console.log(err)
-                                    )
 
-    constructor(private eventService: EventService) {
-        // Do stuff
+    public uploadImage = {
+        id: null,
+        image: null,
+        url: null,
+        source: null
     }
+    
+    public addNews = (id, url) => {
+        this.eventService.addNews(id, url)
+            .subscribe(
+                data => {
+                    this.add.id = null;
+                    this.add.url = null;
+                },
+                err => console.log(err)
+            );
+    }
+
+    public upload = (id: string, imageSource: string, sourceUrl: string) => {
+        let file = document.getElementById('image').files[0];
+        this.uploadService.upload(file)
+            .then(result => {
+                this.eventService.uploadHeaderImage(id, result.data.Key, imageSource, sourceUrl)
+                    .subscribe(
+                        data => {
+                            console.log(data);
+                        }
+                    )
+            });
+    }
+
+    constructor(private eventService: EventService, private uploadService: UploadService) { }
 
     ngOnInit() {
     }
