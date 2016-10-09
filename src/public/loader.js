d809c4d41e3ede55a111871d5ab8d732ccb8c2d9
IncredibLink <incrediblelink@gmail.com>
2016-10-09 10:45:13 +0800

    完善 Loader

--- src/public/loader.js
+++ src/public/loader.js
@@ -1,8 +1,8 @@
 var counter = 0;
 
 function loaded() {
-    if (++counter >= 1) {
+    if (++counter >= 3) {
         document.getElementsByClassName('loader')[0].setAttribute("hidden", true);
-        document.getElementById("content").setAttribute("hidden", false);
+        document.getElementById("content").setAttribute("style", "display: initial");
     }
 }
