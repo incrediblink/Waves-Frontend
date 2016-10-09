d897aa293aaebc5415defe301b251e9297e7d9c4
IncredibLink <incrediblelink@gmail.com>
2016-10-09 10:59:26 +0800

    压缩 loader 文件

--- src/public/loader.js
+++ src/public/loader.js
@@ -1,8 +1,1 @@
-var counter = 0;
-
-function loaded() {
-    if (++counter >= 3) {
-        document.getElementsByClassName('loader')[0].setAttribute("hidden", true);
-        document.getElementById("content").setAttribute("style", "display: initial");
-    }
-}
+function loaded(){++counter<3||(document.getElementsByClassName("loader")[0].setAttribute("hidden",!0),document.getElementById("content").setAttribute("style","display: initial"))}var counter=0;
